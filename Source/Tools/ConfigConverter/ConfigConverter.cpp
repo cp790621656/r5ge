e7c76d8e80941957c5ee05ca040958c5ae91e9aa
arenmook <arenmook@localhost>
2010-06-12 05:40:45 +0000

    _Tools_ *NEW* - Created a new tool -- ConfigConverter. It can be used to convert files between R5A, R5B, and R5C types.

---
+++ Source/Tools/ConfigConverter/ConfigConverter.cpp
@@ -0,0 +1,89 @@
+//============================================================================================================
+//                  R5 Engine, Copyright (c) 2007-2010 Michael Lyashenko. All rights reserved.
+//											www.nextrevision.com
+//============================================================================================================
+// ConfigConverter can be used to convert R5A and R5B files into R5C and vice versa
+//============================================================================================================
+
+#include "../../Engine/Serialization/Include/_All.h"
+using namespace R5;
+
+//============================================================================================================
+
+#define LOAD_DONE	printf("Loaded %s\n", filename.GetBuffer())
+#define SAVE_DONE	printf("Saved out '%s'\n", filename.GetBuffer())
+#define READ_ERROR	printf("ERROR: Unable to read '%s'\n", filename.GetBuffer())
+#define WRITE_ERROR printf("ERROR: Unable to write '%s'\n", filename.GetBuffer())
+
+//============================================================================================================
+// Main application entry point
+//============================================================================================================
+
+int main (int argc, char* argv[])
+{
+#ifdef _MACOS
+	String path ( System::GetPathFromFilename(argv[0]) );
+	System::SetCurrentPath(path.GetBuffer());
+	System::SetCurrentPath("../../../");
+#endif
+
+	if (argc > 1)
+	{
+		TreeNode root;
+
+		for (int i = 1; i < argc; ++i)
+		{
+			String filename (argv[i]);
+
+			if (root.Load(filename))
+			{
+				LOAD_DONE;
+
+				if (filename.EndsWith(".r5a"))
+				{
+					filename.Replace(".r5a", ".r5b");
+				}
+				else if (filename.EndsWith(".r5b"))
+				{
+					filename.Replace(".r5b", ".r5c");
+				}
+				else
+				{
+					String ext (System::GetExtensionFromFilename(filename));
+
+					if (ext.IsValid())
+					{
+						filename.Replace(ext, "r5a", true);
+					}
+					else
+					{
+						filename << ".r5a";
+					}
+				}
+
+				if (root.Save(filename.GetBuffer()))
+				{
+					SAVE_DONE;
+				}
+				else
+				{
+					WRITE_ERROR;
+				}
+			}
+			else
+			{
+				READ_ERROR;
+			}
+		}
+	}
+	else
+	{	
+		puts("Usage: ConfigConverter file0 [file1] [file2] [...]");
+		puts("Example 1: FontConverter HelloWorld.r5a");
+		puts("Example 2: FontConverter HelloWorld.r5b HelloWorld.r5c");
+		puts("You can also drag the file in question onto this executable in order to convert it.");
+	}
+	puts("Press Enter to exit...");
+	getchar();
+	return 0;
+}
