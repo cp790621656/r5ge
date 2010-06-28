7db7f0756325f96f51708a324b6e0ea8d5edea50
arenmook <arenmook@localhost>
2010-06-28 21:01:46 +0000

    _Samples_ - Testing file access in Dev0.

--- Source/Samples/Dev0/Source/Dev0.cpp
+++ Source/Samples/Dev0/Source/Dev0.cpp
@@ -8,81 +8,65 @@
 #include "../../../Engine/Serialization/Include/_All.h"
 using namespace R5;
 
-//============================================================================================================
-
-struct Vertex
-{
-	Vector3f pos;
-	Vector3f normal;
-	Vector2f texCoord;
-
-	bool Matches (const Vector3f& p, const Vector3f& n, const Vector2f& tc)
-	{
-		return (p == pos && n == normal && tc == texCoord);
-	}
-
-	void Set (const Vector3f& p, const Vector3f& n, const Vector2f& tc)
-	{
-		pos = p;
-		normal = n;
-		texCoord = tc;
-	}
-};
+#ifdef _WINDOWS
+#include <windows.h>
+#else
+#include <dirent.h>
+#endif
 
 //============================================================================================================
-
-struct Index
-{
-	uint pi;
-	uint ni;
-	uint ti;
-
-	Index() : pi(0), ni(0), ti(0) {}
-};
-
+// GNU version
 //============================================================================================================
 
-ushort Add (Array<Vertex>& verts, const Vector3f& p, const Vector3f& n, const Vector2f& tc)
+bool ReadFolder (const String& dir, Array<String>& folders, Array<String>& files)
 {
-	FOREACH(i, verts)
+	String file;
+
+#ifdef _WINDOWS
+	WIN32_FIND_DATA info;
+	void* fileHandle = FindFirstFile((dir + "/*.*").GetBuffer(), &info);
+
+	if (fileHandle != INVALID_HANDLE_VALUE)
 	{
-		if (verts[i].Matches(p, n, tc))
+		do 
 		{
-			return (ushort)i;
+			file = info.cFileName;
+
+			if (!file.BeginsWith("."))
+			{
+				if ((info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)
+				{
+					folders.Expand() = file;
+				}
+				else
+				{
+					files.Expand() = file;
+				}
+			}
+		}
+		while (FindNextFile(fileHandle, &info));
+	}
+
+#else
+	DIR* dp = opendir(dir.GetBuffer());
+	if (dp == 0) return false;
+	struct dirent* dirp = 0;
+
+	while ((dirp = readdir(dp)) != 0)
+	{
+		if (dirp->d_type == DT_DIR)
+		{
+			folders.Expand() = dirp->d_name;
+			if (folders.Back().BeginsWith(".")) folders.Shrink();
+		}
+		else
+		{
+			files.Expand() = dirp->d_name;
 		}
 	}
-	verts.Expand().Set(p, n, tc);
-	return (ushort)(verts.GetSize() - 1);
-}
-
-//============================================================================================================
-
-void Finish (Array<Vertex>& verts, Array<ushort>& idx, bool save)
-{
-	static uint counter (0);
-	static TreeNode root ("Root");
-	static TreeNode& core = root.AddChild("Core");
-
-	TreeNode& mesh = core.AddChild("Mesh", String("Weapon %u", counter++));
-
-	Array<Vector3f>& vout = mesh.AddChild("Vertices").mValue.ToVector3fArray();
-	Array<Vector3f>& nout = mesh.AddChild("Normals").mValue.ToVector3fArray();
-	Array<Vector2f>& tout = mesh.AddChild("TexCoords 0").mValue.ToVector2fArray();
-
-	mesh.AddChild("Triangles", idx);
-
-	FOREACH(i, verts)
-	{
-		Vertex& v = verts[i];
-		vout.Expand() = v.pos;
-		nout.Expand() = v.normal;
-		tout.Expand() = v.texCoord;
-	}
-
-	verts.Clear();
-	idx.Clear();
-
-	if (save) root.Save("weapons.r5a");
+    closedir(dp);
+#endif
+    return true;
 }
 
 //============================================================================================================
@@ -98,117 +82,47 @@ int main (int argc, char* argv[])
 #endif
 	System::SetCurrentPath("../../../Resources/");
 
-	String pack;
+	Array<String> folders;
+	Array<String> files;
 
-	if (pack.Load("c:/temp/weaponpack/weapons.obj"))
+	ReadFolder(".", folders, files);
+
+	folders.Sort();
+	files.Sort();
+
+	FOREACH(i, folders)
 	{
-		uint offset(0), stage(0);
-		String line, word;
-
-		// Raw data
-		Array<Vector3f> rawV, rawN;
-		Array<Vector2f> rawTC;
-		Array<Index> rawI;
-
-		// Final data
-		Array<Vertex> verts;
-		Array<ushort> idx;
-		
-		for (;;)
-		{
-			offset = pack.GetLine(line, offset);
-
-			if (line.IsEmpty())
-			{
-				if (stage == 1)
-				{
-					stage = 0;
-					Finish(verts, idx, true);
-				}
-				break;
-			}
-
-			if (line[0] == 'v')
-			{
-				if (stage == 1)
-				{
-					stage = 0;
-					Finish(verts, idx, false);
-				}
-
-				if (line[1] == ' ')
-				{
-					Vector3f& v = rawV.Expand();
-
-					if (sscanf(line.GetBuffer(), "v %f %f %f", &v.x, &v.y, &v.z) != 3)
-					{
-						printf("Parsing error\n");
-					}
-				}
-				else if (line[1] == 't')
-				{
-					Vector2f& v = rawTC.Expand();
-
-					if (sscanf(line.GetBuffer(), "vt %f %f", &v.x, &v.y) != 2)
-					{
-						printf("Parsing error\n");
-					}
-				}
-				else if (line[1] == 'n')
-				{
-					Vector3f& v = rawN.Expand();
-
-					if (sscanf(line.GetBuffer(), "vn %f %f %f", &v.x, &v.y, &v.z) != 3)
-					{
-						printf("Parsing error\n");
-					}
-				}
-			}
-			else if (line[0] == 'f' && line[1] == ' ')
-			{
-				stage = 1;
-				rawI.Clear();
-
-				for (uint wordOffset = 2;;)
-				{
-					wordOffset = line.GetWord(word, wordOffset);
-					if (word.IsEmpty()) break;
-
-					Index& index = rawI.Expand();
-
-					if (sscanf(word.GetBuffer(), "%u/%u/%u", &index.pi, &index.ti, &index.ni) != 3)
-					{
-						printf("Parsing error\n");
-					}
-					else
-					{
-						// OBJ's index is 1-based
-						--index.pi;
-						--index.ti;
-						--index.ni;
-					}
-				}
-
-				if (rawI.GetSize() == 3 || rawI.GetSize() == 4)
-				{
-					idx.Expand() = Add(verts, rawV[rawI[0].pi], rawN[rawI[0].ni], rawTC[rawI[0].ti]);
-					idx.Expand() = Add(verts, rawV[rawI[1].pi], rawN[rawI[1].ni], rawTC[rawI[1].ti]);
-					idx.Expand() = Add(verts, rawV[rawI[2].pi], rawN[rawI[2].ni], rawTC[rawI[2].ti]);
-				
-					if (rawI.GetSize() == 4)
-					{
-						idx.Expand() = Add(verts, rawV[rawI[2].pi], rawN[rawI[2].ni], rawTC[rawI[2].ti]);
-						idx.Expand() = Add(verts, rawV[rawI[3].pi], rawN[rawI[3].ni], rawTC[rawI[3].ti]);
-						idx.Expand() = Add(verts, rawV[rawI[0].pi], rawN[rawI[0].ni], rawTC[rawI[0].ti]);
-					}
-				}
-				else
-				{
-					printf("Unexpected number of vertex entries\n");
-				}
-			}
-		}
+		printf("[%s]\n", folders[i].GetBuffer());
 	}
+
+	FOREACH(i, files)
+	{
+		printf("%s\n", files[i].GetBuffer());
+	}
+
+#ifdef _WINDOWS
+	OPENFILENAME ofn;
+	char szFile[260];
+	szFile[0] = 0;
+
+	// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(OPENFILENAME));
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = 0;
+	ofn.lpstrFile = szFile;
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = "All\0*.*\0";
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+	if (GetOpenFileName(&ofn))
+	{
+		printf("File: %s\n", ofn.lpstrFile);
+	}
+#endif
 	printf("Done!\n");
 	getchar();
 	return 0;
