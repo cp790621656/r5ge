4e83be5d4a30befc294deee6dc01708f5a9554c4
arenmook <arenmook@localhost>
2010-08-05 14:13:31 +0000

    _UI_ *FIX* - Menu drop-down should now hide properly as well.

--- Source/Engine/UI/Include/UIContext.h
+++ Source/Engine/UI/Include/UIContext.h
@@ -71,7 +71,7 @@ protected:
 
 	// Event callback for a visual highlight
 	void _OnMouseMove	(UIWidget* widget, const Vector2i& pos, const Vector2i& delta);
-	void _OnFocusChange (UIWidget* widget, bool hasFocus) { if (!hasFocus) SetAlpha(0.0f, 0.25f); }
+	void _OnFocusChange (UIWidget* widget, bool hasFocus) { if (!hasFocus) SetAlpha(0.0f, 0.25f); OnFocus(false); }
 	void _OnKeyPress	(UIWidget* widget, const Vector2i& pos, byte key, bool isDown);
 
 public:
