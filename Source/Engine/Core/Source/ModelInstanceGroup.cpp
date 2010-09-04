6b51c1b1e27cc3d8a1d6bc37108ccbf7cd418f61
arenmook <arenmook@localhost>
2010-09-04 16:27:23 +0000

    _Core_ *FIX* - Removed DotC's copyright message from the file that has since been moved to Core.

--- Source/Engine/Core/Source/ModelInstanceGroup.cpp
+++ Source/Engine/Core/Source/ModelInstanceGroup.cpp
@@ -1,8 +1,3 @@
-//============================================================================================================
-//  Defense of the Citadel, Copyright (c) 2010 Michael Lyashenko and Philip Cosgrave. All rights reserved.
-//											www.nextrevision.com
-//============================================================================================================
-
 #include "../Include/_All.h"
 using namespace R5;
 
