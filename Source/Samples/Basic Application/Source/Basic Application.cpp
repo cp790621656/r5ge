074faf6db1f2971ea416f0f734359db86648c3fc
arenmook <arenmook@localhost>
2011-01-06 18:43:23 +0000

    _Samples_ - Checking in a sample for Unity.

--- Source/Samples/Basic Application/Source/Basic Application.cpp
+++ Source/Samples/Basic Application/Source/Basic Application.cpp
@@ -1,6 +1,35 @@
 #include "../Include/Basic Application.h"
 using namespace R5;
 
+Core* g_core = 0;
+
+//============================================================================================================
+
+class UITest : public UIScript
+{
+public:
+
+	R5_DECLARE_INHERITED_CLASS("UITest", UITest, UIScript, UIScript);
+
+	virtual void OnStateChange (uint state, bool isSet)
+	{
+		if (state == UIButton::State::Pressed && isSet)
+		{
+			Object* parent	= g_core->GetRoot()->FindObject<Object>("Teapot");
+			Object* child	= g_core->GetRoot()->FindObject<Object>("Teapot 2");
+
+			if (child->GetParent() == parent)
+			{
+				child->SetParent(0);
+			}
+			else
+			{
+				child->SetParent(parent);
+			}
+		}
+	}
+};
+
 //============================================================================================================
 
 TestApp::TestApp() : mWin(0), mGraphics(0), mUI(0), mCore(0)
@@ -9,6 +38,10 @@ TestApp::TestApp() : mWin(0), mGraphics(0), mUI(0), mCore(0)
 	mGraphics	= new GLGraphics();
 	mUI			= new UI(mGraphics, mWin);
 	mCore		= new Core(mWin, mGraphics, mUI);
+
+	g_core = mCore;
+
+	UIScript::Register<UITest>();
 }
 
 //============================================================================================================
